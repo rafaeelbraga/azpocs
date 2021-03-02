462162cbd1412f0cbe4667265f59ae2f5a657c7e
Rafael Braga <rafaeel.braga@gmail.com>
2021-03-01 21:38:31 -0300

    feat: criação de azure functions com trigger utilizando kafka

---
+++ src/KafkaTriggerPoc/KafkaTriggerPoc.cs
@@ -0,0 +1,15 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Kafka;
+using Microsoft.Extensions.Logging;
+
+namespace KafkaTriggerPoc
+{
+    public static class KafkaTriggerPoc
+    {
+        [FunctionName("KafkaTriggerPoc")]
+        public static void Run([KafkaTrigger("BrokerKafka", "kafka-topic", ConsumerGroup = "kafka-poc-consumer")]KafkaEventData<string> kafkaEvent, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus topic trigger function processed message: {kafkaEvent.Value}");
+        }
+    }
+}
