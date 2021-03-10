f434de4519fd584967f10cae333a7c0cff96cf74
Rafael Braga <rafaeel.braga@gmail.com>
2021-03-10 20:56:00 -0300

    refactor: adicionando broker e topicos em variavel de ambiente

--- src/KafkaTriggerPoc/KafkaTriggerPoc.cs
+++ src/KafkaTriggerPoc/KafkaTriggerPoc.cs
@@ -7,7 +7,7 @@ namespace KafkaTriggerPoc
     public static class KafkaTriggerPoc
     {
         [FunctionName("KafkaTriggerPoc")]
-        public static void Run([KafkaTrigger("BrokerKafka", "kafka-topic", ConsumerGroup = "kafka-poc-consumer")]KafkaEventData<string> kafkaEvent, ILogger log)
+        public static void Run([KafkaTrigger("BrokerKafka", "kafka-topic", ConsumerGroup = "kafka-poc-consumer")] KafkaEventData<string> kafkaEvent, ILogger log)
         {
             log.LogInformation($"C# ServiceBus topic trigger function processed message: {kafkaEvent.Value}");
         }
